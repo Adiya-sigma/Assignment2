336730ab408e251dfc9db718980d22968a9a7857
Adiya-sigma <adiyamedihanova@gmail.com>
2025-03-01 20:36:19 +0500

    Second Commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,18 +8,10 @@ public class Main {
     public static void main(String[] args) {
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(ProjectConfig.class);
 
-        LibraryManager libraryManager = context.getBean(LibraryManager.class);
-        LibraryRepository libraryRepository = context.getBean(LibraryRepository.class);
-
-        Book book = new Book();
-        book.setTitle("Java");
-        book.setAuthor("Eldar");
-        book.setId(1);
-
-
-        libraryRepository.addBook(book);
-
-        libraryManager.showAllBooks();
+        ReportGenerator reportGenerator = context.getBean(ReportGenerator.class);
+        reportGenerator.generateReport();
+        reportGenerator.readReport();
+        context.close();
 
 
     }
--- src/main/java/org/example/ReportGenerator.java
+++ src/main/java/org/example/ReportGenerator.java
@@ -9,4 +9,10 @@ public class ReportGenerator {
     public ReportGenerator() {
         System.out.println("Report Generator");
     }
+    public void generateReport() {
+        System.out.println("Report Generator");
+    }
+    public void readReport() {
+        System.out.println("Reading the report");
+    }
 }
